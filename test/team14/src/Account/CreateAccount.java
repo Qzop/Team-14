9afa17889c79ebb3f66b16a4e687a14957109593
Juan <51283162+Juanboy-bebop@users.noreply.github.com>
2021-11-28 19:56:10 -0700

    Update CreateAccount.java
    Created a createID( ) function to generate a unique patient username from patient first and last name and birthday. The function returns a unique ID of type String

--- test/team14/src/Account/CreateAccount.java
+++ test/team14/src/Account/CreateAccount.java
@@ -8,6 +8,7 @@ public class CreateAccount extends Login
 	private String firstname;
 	private String lastname;
 	private String email;
+	private String uniqueID;
 	private long phonenumber;
 	
 	public void createAccount(String birthday, String firstname, String lastname, String email, long phonenumber)
@@ -17,6 +18,19 @@ public class CreateAccount extends Login
 		this.lastname = lastname;
 		this.email = email;
 		this.phonenumber = phonenumber;
+		this.uniqueID = createID(firstname, lastname, birthday);
 	}
-	
+		public String createID(String firstname, String lastname, String bday)
+	{
+		this.firstname = firstname;
+		this.lastname  = lastname;
+		this.birthday  = bday; 
+			
+		String temp = firstname.substring(0, 3);
+		String templast = lastname.substring(0);
+		String tempbday = bday.substring(bday.length() - 2); //get last two numbers for the year
+		
+		String uniqueID = temp.concat(templast).concat(tempbday);
+		return uniqueID;
+	}
 }
